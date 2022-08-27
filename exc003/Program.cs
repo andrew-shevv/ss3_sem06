93d079613d08a763d9b3926d4f6ea06a0cc39709
Andrew <vishnyaishere@gmail.com>
2022-08-28 00:17:46 +0300

    exc003 struggled-through

--- exc003/Program.cs
+++ exc003/Program.cs
@@ -3,3 +3,19 @@
 // 3 -> 11
 // 2 -> 10
 
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] ToBinary(int numDecimal){
+    int size =  ((int)Math.Log2(numDecimal) + 1);
+    int[] numBinary = new int[size];
+    for (int i = size - 1; i >= 0; i--){
+        numBinary[i] = numDecimal % 2;
+        numDecimal /= 2;
+    }
+    return numBinary;
+}
+
+int[] binaryAlley = ToBinary(num);
+
+Console.Write(string.Join("", binaryAlley));
