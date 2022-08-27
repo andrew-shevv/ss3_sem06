583f4eaed629aa32e02063d16ef186f6f66f7e36
Andrew <vishnyaishere@gmail.com>
2022-08-28 01:52:12 +0300

    exc006 done

--- exc006_hw/Program.cs
+++ exc006_hw/Program.cs
@@ -2,3 +2,20 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+Console.Write("How many numbers do you want to input? ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int[] inputArr = new int[x];
+
+int countPos = 0;
+
+for (int i = 0; i < x; i++){
+    Console.Write($"Input number {i + 1} of {x}: ");
+    inputArr[i] = Convert.ToInt32(Console.ReadLine());
+    if (inputArr[i] > 0){
+        countPos++;
+    }
+}
+
+Console.WriteLine('[' + string.Join(", ", inputArr) + ']');
+Console.Write($"You have inputted {countPos} positive numbers");
