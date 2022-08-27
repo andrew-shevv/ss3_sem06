0031f03d16e2ab5369d9cb7062725e48a13ea63f
Andrew <vishnyaishere@gmail.com>
2022-08-27 19:38:23 +0300

    exc001 personal solution

--- exc001/Program.cs
+++ exc001/Program.cs
@@ -3,3 +3,19 @@
 // [1 2 3 4 5] -> [5 4 3 2 1]
 // [6 7 3 6] -> [6 3 7 6]
 
+int[] array = new int[new Random().Next(3, 8)];
+
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++){
+    array[i] = rand.Next(1, 10);
+}
+
+Console.Write('[' + string.Join(", ", array) + ']' + " ==> ");
+
+for (int i = 0; i < array.Length / 2; i++){
+    int temp = array[i];
+    array[i] = array[array.Length - i - 1];
+    array[array.Length - i - 1] = temp;
+}
+
+Console.Write('[' + string.Join(", ", array) + ']');
