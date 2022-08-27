5860afac2ba261872301a06336e648d90b88b3dd
Andrew <vishnyaishere@gmail.com>
2022-08-27 20:05:19 +0300

    exc002 done

--- exc002/Program.cs
+++ exc002/Program.cs
@@ -2,3 +2,16 @@
 // и проверяет, может ли существовать треугольник с сторонами такой длины.
 // Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
 
+Console.Write("Input side x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input side y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input side z: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Can a triangle with sides: {x}, {y}, {z}, exist?");
+
+if (x < y + z && y < x + z && z < x + y){
+    Console.Write(" -- yes");
+}
+else Console.Write(" -- no");
