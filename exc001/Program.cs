867344d81fed6c90f6bda9aed33da6853ab7d1e4
Andrew <vishnyaishere@gmail.com>
2022-08-27 19:56:18 +0300

    exc001 done nicely

--- exc001/Program.cs
+++ exc001/Program.cs
@@ -3,19 +3,31 @@
 // [1 2 3 4 5] -> [5 4 3 2 1]
 // [6 7 3 6] -> [6 3 7 6]
 
-int[] array = new int[new Random().Next(3, 8)];
-
-Random rand = new Random();
-for (int i = 0; i < array.Length; i++){
-    array[i] = rand.Next(1, 10);
+int[] NewArray(int len, int min, int max){
+    int[] array = new int[len];
+    Random rand = new Random();
+    for (int i = 0; i < len; i++){
+        array[i] = rand.Next(min, max + 1);
+    }
+    return array;
 }
 
-Console.Write('[' + string.Join(", ", array) + ']' + " ==> ");
-
-for (int i = 0; i < array.Length / 2; i++){
-    int temp = array[i];
-    array[i] = array[array.Length - i - 1];
-    array[array.Length - i - 1] = temp;
+void Reverse(int[] array){
+    for (int i = 0; i < array.Length / 2; i++){
+        int temp = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temp;
+    }
 }
 
-Console.Write('[' + string.Join(", ", array) + ']');
+const int SIZE = 7;
+const int MIN_VAL = -9;
+const int MAX_VAL = 9;
+
+int[] list = NewArray(SIZE, MIN_VAL, MAX_VAL);
+
+Console.Write('[' + string.Join(", ", list) + ']' + " ==> ");
+
+Reverse(list);
+
+Console.Write('[' + string.Join(", ", list) + ']');
